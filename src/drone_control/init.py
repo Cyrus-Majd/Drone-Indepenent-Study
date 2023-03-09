df4fb8010d12467448909ae898920dd7f79ca4cb
Mohamad Khawam <55813440+dominusmars@users.noreply.github.com>
2023-03-08 23:52:13 -0500

    Drone Control Functions

---
+++ src/drone_control/init.py
@@ -0,0 +1,21 @@
+class Drone_Control:
+    def __init__(self, multiplier):
+        pass
+
+    def climb(self, amount):
+        pass
+
+    def descending(self, amount):
+        pass
+
+    def turn_right(self, radius):
+        pass
+
+    def turn_left(self, radius):
+        pass
+
+    def forwards(self, amount):
+        pass
+
+    def back(self, amount):
+        pass
