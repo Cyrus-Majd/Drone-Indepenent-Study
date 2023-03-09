a3c899a3bfeb60b56784d68f715ae992ffe41822
Mohamad Khawam <55813440+dominusmars@users.noreply.github.com>
2023-03-09 00:39:05 -0500

    Added drone print outs

--- src/drone_control/init.py
+++ src/drone_control/init.py
@@ -1,21 +1,30 @@
 class Drone_Control:
     def __init__(self, multiplier):
+        print("I BREATHE")
         pass
 
     def climb(self, amount):
+        print("climbing " + amount)
         pass
 
     def descending(self, amount):
+        print("descending " + amount)
         pass
 
     def turn_right(self, radius):
+        print("turning right" + radius)
         pass
 
     def turn_left(self, radius):
+        print("turning left" + radius)
         pass
 
     def forwards(self, amount):
+        print("forwarding" + amount)
         pass
 
     def back(self, amount):
+        print("backwardsing" + amount)
         pass
+
+    print("i am mohamad")
